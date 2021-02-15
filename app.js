cdbb0c5a361d6e56f740489988a2c4f0913e36af
Sander Odekerken <odekerkensander@gmail.com>
2021-02-15 11:24:47 +0100

    Complete FAQ challenge

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 $(function () {
   $("h2").click(function () {
+    
     if ($(this).next("p").is(":visible")) {
       $("p").hide();
+      $("span").css({'transform': 'rotate(0deg)'});
     } else {
       $("p").hide();
       $("h2").css("font-weight", "400");
       $(this).next("p").show();
+      $(this).find("span").css({'transform': 'rotate(-180deg)'});
     }
 
     if ($(this).css("font-weight") == "400") {
